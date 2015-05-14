9dcb5e4a4187f2fe7c9ac466192ebc60dd4a0380
Henry Katz <henry.katz@iscs-i.com>
2015-05-14 13:53:22 -0400

    sync w/ github

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -71,7 +71,6 @@ object Application extends Controller with myTypes {
     "from Member m, Uzer u " +
     "where m.uid = u.id and " +
     "u.username = :username"
-
   val myCache:Memcached = Shared.memd.get
   val minDuration:Duration = 1.milli
   val maxDuration:Duration = 2.milli
