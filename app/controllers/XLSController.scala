82d7c4ce6c69fd741da3f4c6452fc95e09f7384a
Henry Katz <henry.katz@iscs-i.com>
2016-03-27 13:12:27 -0400

    Issue with XLS file download was ISO case.

--- app/controllers/XLSController.scala
+++ app/controllers/XLSController.scala
@@ -1,10 +1,12 @@
 package controllers
 
-import java.io.File
+import java.nio.charset.CodingErrorAction
 
-import play.api.Environment
+import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.Json
 import play.api.mvc._
+
+import scala.io.Source
 
 /**
  * Created by hkatz on 3/26/16.
@@ -12,18 +14,38 @@ import play.api.mvc._
 class XLSController extends Controller {
 
   def sendTemplate(tempType: String) = Action {
-    val myEnv = new Environment(new File("."), this.getClass.getClassLoader, play.api.Mode.Dev)
-    val (tempFile, tempName) = tempType match {
-      case "C" => (myEnv.getFile("public/contacts_template.xls"), "contact.xls")
-      case "E" => (myEnv.getFile("public/expenses_template.xls"), "template.xls")
-      case "_" => (myEnv.getFile("public/contacts_template.xls"), "BAD")
-    }
+    val URLBASE = "http://wengen/DOWNLOAD"
+    val (templateContent, tempName) =
+      try {
+        val tempFile = tempType match {
+          case "C" => "contacts_template.xls"
+          case "E" => "expenses_template.xls"
+          case "_" => "BAD"
+        }
+        implicit val codec = scala.io.Codec("ISO-8859-1")
+
+        codec.onMalformedInput(CodingErrorAction.IGNORE)
+        codec.onUnmappableCharacter(CodingErrorAction.IGNORE)
+        val data = Source.fromURL(s"${URLBASE}/$tempFile")
+        val asStr = data.mkString
+        (asStr, tempFile)
+      } catch {
+        case e: java.nio.charset.MalformedInputException =>
+          (s"template not found ${e.printStackTrace}", "BAD")
+        case e: java.io.IOException =>
+          (s"template not found ${e.printStackTrace}", "BAD")
+      }
+    val tempEnum = Enumerator(templateContent.toCharArray.map(_.toByte))
     if (tempName == "BAD")
       Ok(Json.obj("request" -> "incorrect type"))
     else
-      Ok.sendFile(
-        content = tempFile,
-        fileName = _ => tempName
+      Result(
+        header = ResponseHeader(200,
+          Map(
+            CONTENT_LENGTH -> templateContent.size.toString,
+            CONTENT_DISPOSITION -> s"attachment; filename=$tempName"
+          )) ,
+        body = tempEnum
       )
   }
 }
