68bb8050edac32b1b4bcd1c67bd02985663769af
Henry Katz <henry.katz@iscs-i.com>
2016-03-31 12:42:18 -0400

    Starting tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,9 +1,14 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
+import play.api.libs.json._
 import play.api.test._
+import play.api.libs.ws._
 import play.api.test.Helpers._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
 
 /**
  * Add your spec here.
@@ -13,18 +18,47 @@ import play.api.test.Helpers._
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
+  val getUserUrl = "http://berne:8880/expense/get/User/9"
+
+  case class Uzer(activation: String, active: String, active_timestamp: String, id: Int, joined_date: String,
+                  nodata: String, password: String, role: String, username: String)
+
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
-    }
+    /*"send 404 on a bad request" in new WithApplication{
+      val testRet = FakeRequest(GET, "/boum", FakeHeaders() ,body = "", remoteAddress = "wengen:8880")
+      route(testRet).get mustNotEqual beNone
+    }*/
 
     "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
+      implicit val personReads = Json.reads[Uzer]
+      val wsRespF: Future[WSResponse] = WS.url(getUserUrl).get()
+      val resultF: Future[JsResult[Uzer]] = wsRespF.map {
+        response =>
+            response.json.validate[Uzer]
+          //(response.json \ "username").as[String]
+      }
+      val wsResp = Await.result(wsRespF.mapTo[WSResponse].map { resp => resp}, 5.milli)
+      val uzerJS = Await.result(resultF, 700.milli)
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      val (uzer, valid) = uzerJS match {
+        case u: JsSuccess[Uzer] => (u.get, true)
+        case e: JsError => (e.get, false)
+      }
+
+      val contentType = wsResp.header("content-type")
+
+      if (contentType.isDefined)
+        contentType.get.split(";")(0) must equalTo("application/json")
+
+      if (valid)
+        uzer.username must equalTo("johngalt")
+      wsResp.status must equalTo(OK)
+
+      //val home = route(FakeRequest(GET, "/expense/get/User/9", FakeHeaders(), body = "", remoteAddress = "wengen:8880")).get
+
+      //contentType(home) must beSome.which(_ == "application/json")
+      //contentAsString(home) must contain ("activation")
     }
   }
 }
